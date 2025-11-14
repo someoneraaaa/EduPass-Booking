7f83b327fd7ce8e3185e816da170b5ffbe0487bd
someoneraaaa <meiram.yerassyl@gmail.com>
2025-11-14 12:44:01 +0500

    Delete app directory

--- app/di.py
+++
@@ -1,7 +0,0 @@
-from app.infrastructure.db import db
-from app.adapters.repo.mongo_booking_repo import MongoBookingRepository
-from app.usecase.booking import BookingUseCase
-
-def get_booking_usecase() -> BookingUseCase:
-    repo = MongoBookingRepository(db)
-    return BookingUseCase(repo)
--- app/settings.py
+++
@@ -1,7 +0,0 @@
-from pydantic_settings import BaseSettings
-
-class Settings(BaseSettings):
-    MONGO_URL: str = "mongodb://localhost:27017"
-    MONGO_DB: str = "edupass_booking"
-
-settings = Settings()
